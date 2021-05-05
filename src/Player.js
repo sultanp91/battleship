578fa02083ce34b5c15d55f3a918e21cd4b21259
Sultan <sultanp@gmail.com>
2021-05-05 17:42:13 +0100

    updated player factory

--- src/Player.js
+++ src/Player.js
@@ -1,3 +1,20 @@
-export default playerFactory () => {
-    
+import gameboardFactory from './Gameboard';
+
+export default function playerFactory(name, turn) {
+  const name = name;
+
+  let board = gameboardFactory();
+
+  let turn = turn;
+
+  const randomMove = () => {
+    let randomIdx = Math.floor(Math.random() * 100);
+
+    if (board.boardArray[randomIdx].hit === false) {
+      return randomIdx;
+    } else if (board.boardArray[randomIdx].hit === true) {
+      randomIdx = randomMove();
+    }
+    return randomIdx;
+  };
 }
